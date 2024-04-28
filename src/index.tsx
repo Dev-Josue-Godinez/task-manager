798e097147fa63a4c6f53a3db57876a7486f403f
Josue Godinez <josuegod1998.dev@gmail.com>
2024-04-28 17:12:00 -0600

    Aplied some corrections

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Login from './components/Login/Login';
+import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Login />
+    <App />
   </React.StrictMode>
 );
 

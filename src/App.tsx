c5f1ded9a78bd04d986a973d9b090fdc363b9d97
Josue Godinez <josuegod1998.dev@gmail.com>
2024-05-04 15:32:07 -0600

    Minimal Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login/Login';
 
+
 function App() {
   return (
-    <Login></Login>
+    <><Login></Login></>
   );
 }
 
